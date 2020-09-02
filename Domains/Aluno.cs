b29b406f86bf8856f6b49eedcb4e3f957dc2e69c
unknown <villagepllayer@gmail.comgit co>
2020-09-02 17:57:16 -0300

    Finalizado

---
+++ Domains/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Domains
+{
+    public class Aluno
+    {
+        public int Idaluno { get; set; }
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+    }
+}
